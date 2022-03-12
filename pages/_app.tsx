19a5ea80b26c54e477607a27d2d4d049c93978ca
Dmitry Neklyudov <dima.kazanec.394@yandex.ru>
2022-03-12 17:35:00 +0300

    commit 37

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,33 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from "next/head";
+import ym, {YMInitializer} from 'react-yandex-metrika';
 
 function MyApp({ Component, pageProps, router }: AppProps) {
+  router.events.on('routeChangeComplete', (url: string) => {
+    if (typeof window !== 'undefined') {
+      ym('hit', url);
+    }
+  });
+
   return (
     <>
       <Head>
         <title>My first NextJS app</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link rel="preconnect" href="https://mc.yandex.ru" />
         {/* eslint-disable-next-line @next/next/no-page-custom-font */}
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap"
           rel="stylesheet" />
         <meta property="og:url" content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath} />
         <meta property="og:locale" content="ru_RU"/>
       </Head>
+      <YMInitializer
+        accounts={[]}
+        options={{webvisor: true, defer: true}}
+        version="2"
+      />
       <Component {...pageProps} />
     </>
 
